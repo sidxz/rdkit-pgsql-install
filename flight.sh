a47f72c710fe3f558d169415e07c5c4dc73521a9
Siddhant Rath <sid@tamu.edu>
2024-06-04 18:26:54 -0500

    flight improved

--- flight.sh
+++ flight.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+  echo "Docker is not installed. Please install Docker first."
+  exit 1
+fi
+
+
 # Check if .env file exists
 if [ ! -f .env ]; then
   echo ".env file not found!"
@@ -7,7 +14,9 @@ if [ ! -f .env ]; then
 fi
 
 # Export environment variables from .env file
-export $(grep -v '^#' .env | xargs)
+set -a
+source .env
+set +a
 
 # Check if POSTGRES_USER and POSTGRES_PASSWORD are set
 if [ -z "$POSTGRES_USER" ] || [ -z "$POSTGRES_PASSWORD" ]; then
@@ -16,13 +25,23 @@ if [ -z "$POSTGRES_USER" ] || [ -z "$POSTGRES_PASSWORD" ]; then
 fi
 
 # Check if /data directory exists
-if [ ! -d /data ]; then
-  echo "/data directory does not exist!"
+if [ ! -d /data/moldb ]; then
+  echo "/data/moldb directory does not exist!"
+  exit 1
+fi
+
+# Check if docker-compose.yml file exists
+if [ ! -f docker-compose.yml ]; then
+  echo "docker-compose.yml file not found!"
   exit 1
 fi
 
 # Start Docker Compose
 echo "Starting Docker Compose..."
 docker compose -f docker-compose.yml up --force-recreate -d
+if [ $? -ne 0 ]; then
+    echo "Failed to start Docker Compose."
+    exit 1
+fi
 
 echo "Docker Compose started successfully."
